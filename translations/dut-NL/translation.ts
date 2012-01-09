69991fce8c6689dae206082962f76a6eeb8d52cd
svda <sander@myxt.nl>
2012-01-09 18:14:07 +0100

    Added Dutch translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL" sourcelanguage="en_GB">
+<context>
+    <name>design/admin/popupmenu</name>
+    <message>
+        <source>Push node</source>
+        <translation>Push item</translation>
+    </message>
+</context>
+<context>
+    <name>ngpush/status</name>
+    <message>
+        <source>Please wait, requesting...</source>
+        <translation>Verzoek versturen...</translation>
+    </message>
+    <message>
+        <source>This entry has already been pushed.</source>
+        <translation>Dit item is al gepushed.</translation>
+    </message>
+    <message>
+        <source>Message is too long!</source>
+        <translation>Bericht te lang.</translation>
+    </message>
+    <message>
+        <source>Waiting...</source>
+        <translation>Een moment geduld...</translation>
+    </message>
+    <message>
+        <source>You have %number characters remaining.</source>
+        <translation>%number tekens over.</translation>
+    </message>
+    <message>
+        <source>Push</source>
+        <translation>Item pushen</translation>
+    </message>
+    <message>
+        <source>Push to all services</source>
+        <translation>Naar alle sociale media pushen</translation>
+    </message>
+    <message>
+        <source>No defined push services.</source>
+        <translation>Geen sociale media gedefinieerd.</translation>
+    </message>
+</context>
+<context>
+    <name>ngpush/ui</name>
+    <message>
+        <source>Close this window</source>
+        <translation>Scherm sluiten</translation>
+    </message>
+    <message>
+        <source>Insert hashtags</source>
+        <translation>Hashtag invoeren</translation>
+    </message>
+    <message>
+        <source>Insert full link</source>
+        <translation>Volledige link invoeren</translation>
+    </message>
+    <message>
+        <source>Insert short link</source>
+        <translation>Verkorte link invoeren</translation>
+    </message>
+    <message>
+        <location filename="save_access_token.php" line="37"/>
+        <source>Access token has been successfully obtained and saved.%brYou can continue using your Netgen Push application.</source>
+        <translation>Toegangstoken vergregen en opgeslagen.%brU kunt het item nu pushen.</translation>
+    </message>
+    <message>
+        <location filename="save_access_token.php" line="41"/>
+        <source>An error has occured.%brYour access token could not be saved, please contact your Netgen Push administrator.</source>
+        <translation>Er is een fout opgetreden.%brUw toegangstoken kon niet worden opgeslagen, neem contact op met de beheerder.</translation>
+    </message>
+</context>
+</TS>
